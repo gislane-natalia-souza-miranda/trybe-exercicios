19074d3ffc6e2048ceece1fd53fe1421c0a2812a
Gislane Natália de Souza Miranda <gislane.natalia.souza.miranda@gmail.com>
2022-03-08 00:27:43 -0300

    Atividades dentro  do Conteudo

---
+++ Modulo01_Fundamentos/Bloco04-JavaScript/atividadePratica.js
@@ -0,0 +1,36 @@
+//Primeira Parte
+
+/* let myName = "Gislane";
+let birthCity = "Ervália";
+let birthYear = 1983;
+
+console.log(myName); 
+console.log(birthCity); 
+console.log(birthYear);  */
+
+//Segunda Parte
+
+/* let patientId = "50";
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientEmail); */
+
+//console.log(typeof patientAge); //R= undefined
+
+// Terceira Parte
+
+const base = 5;
+let height = 8;
+const area = base * height;
+const perimeter = 2*base + 2*height;
+
+console.log(area);
+console.log(perimeter);
